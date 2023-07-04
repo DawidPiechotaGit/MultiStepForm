1e93281deec8b051aefb3f8634f3e6851f814e98
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-07-04 18:07:04 +0200

    logic behind buttons + style

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ const stage = sidebarStep.length;
 let stepNum = 0;
 
 function showStep(x) {
+  if (x === 0) {
+    bckButton.classList.add("hidden");
+  } else if (x === 4) {
+    nxtButton.classList.add("hidden");
+  } else {
+    bckButton.classList.remove("hidden");
+    nxtButton.classList.remove("hidden");
+  }
+
+  if (x === 3) {
+    nxtButton.textContent = "Confirm";
+  }
+
   if (x < formStep.length) {
     for (let i = 0; i < formStep.length; i++) {
       formStep[i].classList.remove("active");
